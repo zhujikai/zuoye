a59c9090750d8db4ba6d71e685dc1e6b1aa56cfa
zhujiaki <1026181407@qq.com>
2017-05-26 11:41:42 +0800

    first

---
+++ zuoye/zuoye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zuoye
+//
+//  Created by zjk on 2017/5/26.
+//  Copyright © 2017年 zhuJiKai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zuoye/zuoye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zuoye
+//
+//  Created by zjk on 2017/5/26.
+//  Copyright © 2017年 zhuJiKai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
